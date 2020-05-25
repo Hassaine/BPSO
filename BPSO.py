5da9b17c109e38940bb0f02c0e30622dc420b4da
hassaine <hassaine24@gmail.com>
2020-05-25 23:15:26 +0100

    change data representation and add more mesure of performance

--- BPSO.py
+++ BPSO.py
@@ -1,15 +1,21 @@
 
-from Particule import Particule
+
+
 from random import random
 import pandas as pd
 import time
 import math
 import numpy as np
-
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir+"\\pso")
+from Particule import Particule
 class BPSO:
-    def __init__(self,df, particule_count=5000,v_max=20, C1=2,C2=2,w_coef=0.4,max_iter=500,n=200,lift=False):
-
-        self.population = [Particule(n=n,v_max=v_max,lift=lift) for _ in range(particule_count)]
+    def __init__(self,df, particule_count=5000,v_max=20, C1=2,C2=2,w_coef=0.4,max_iter=500,n=200,mesure="confidence"):
+        """":arg mesure='confidence', 'lift','leverage', 'conviction' """
+        self.population = [Particule(n=n,v_max=v_max,mesure=mesure) for _ in range(particule_count)]
+        self.mesure="confidence"
         self.df=df
         self.size=n
         self.v_max=v_max
@@ -43,11 +49,13 @@ class BPSO:
 #                if (p.Vid > self.v_max):
 #                    p.Vid =self.v_max
                 if(random() > self.sigmoid(p.Vid)):
+                    #print("iter = {}, done".format(k))
                     p.Vid=1
                     p.updatePosition(self.df)
                 if ( p.pbestFitness > self.gBestfitness):
                     self.gBest = i
                     self.gBestfitness = p.pbestFitness
+
 
     def hamming_distance(self,p1,p2):
         return ((p1 != p2) == True).sum()
@@ -57,18 +65,11 @@ class BPSO:
 
 
 
-
-
-
-if __name__ == '__main__':
-
-    df= pd.read_csv('market.csv')
-    start_time = time.time()
-    m=30
+def association_rule_mining(df,particule_count=5000,v_max=20, C1=2,C2=2,w_coef=0.4,max_iter=5,mesure="confidence",m=10):
     particules=[]
     i=0
     while(i<m):
-        instance = BPSO(df=df,n=2*len(df.columns),particule_count=1000,max_iter=20,lift=False)
+        instance = BPSO(df=df,n=2*len(df.columns),particule_count=particule_count,max_iter=max_iter,mesure=mesure)
         instance.run()
         gbest = instance.getGbest()
         exist = False
@@ -78,13 +79,39 @@ if __name__ == '__main__':
                 break
         if not exist:
             particules.append(gbest)
-            i += 1
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-
+        i += 1
+    output = [["","",0,0,0,0,0]]
     for particule in particules:
         particule.getRule(columns=df.columns)
-        print(" lift = {}".format(particule.fitness(df=df)))
-    #gbest = instance.getGbest()
-    #gbest.getRule(columns=df.columns)
-    #print(" confiance = {}".format(gbest.fitness(df=df)))
+        if(particule.confidence>0):
+            output.append([particule.premis,particule.conclusion,particule.support,particule.confidence,particule.lift,particule.leverage,particule.conviction])
+        #print(" confidence = {}, support = {} lift = {}, leverage  = {}, conviction = {}".format(particule.confidence,particule.support,particule.lift,particule.leverage,particule.conviction))
+    columns=["antecedents","consequents","support","confidence","lift","leverage","conviction"]
+    output_df = pd.DataFrame(output,columns=columns)
+    return output_df
+
+
+
+if __name__ == '__main__':
+    colomns = ['time','transaction_number','max_iter','particule_count','avg_support', 'avg_confidence','avg_lift','avg_leverage','avg_conviction']
+    result = []
+    df= pd.read_csv('../../dataSource/market/marketBool.csv')
+    particule_counts=[100,200,300,500,1000]
+    max_iters=[1,3,5,10]
+    transNumber = [_ for _ in range(980, 10000, 980)]
+    for trans in transNumber:
+        df_tmp = df.sample(n=trans, replace=False)
+        for max_iter in max_iters:
+            for particule_count in particule_counts:
+                start_time = time.time()
+                output = association_rule_mining(df=df_tmp,particule_count=particule_count,max_iter=max_iter,mesure="lift", m=10)
+                endingTime = (time.time() - start_time)
+                print("{},{},{},{}".format(trans,max_iter,particule_count,endingTime))
+                result.append([endingTime,trans,max_iter,particule_count,output['support'].mean(),output['confidence'].mean(),output['lift'].mean(),output['leverage'].mean(),output['conviction'].mean()])
+
+    output = pd.DataFrame(result, columns=colomns)
+    output.to_csv('../../output/dataMining/BPSO_performance.csv', index=False)
+
+    #print(output.to_string())
+    #output.to_csv('../../output/dataMining/BPSO_ar.csv',index=False)
+
--- Particule.py
+++ Particule.py
@@ -5,24 +5,33 @@ import math
 
 class Particule:
 
-    def __init__(self,n,v_max,lift):
+    def __init__(self,n,v_max,mesure):
+        self.mesure=mesure
         self.size=n
         self.position =  np.random.choice(2,n,p=[0.98, 0.02]) # scince 2% of our data is 1 in the dataset this can depend on the data
         # if you are using some small dataset with a lot of 1 then you may use  diffrent parameter for the p
         self.pBest = self.position
         self.Vid = randint(0,v_max)
         self.pbestFitness=0
-        self.lift=lift
 
+        self.support=0
+        self.confidence=0
+        self.lift = 0
+        self.leverage =0
+        self.conviction = 0
+        self.premis=frozenset()
+        self.conclusion=frozenset()
     def fitness(self,df,postion=True):
         # if the particule is a valide  X=>Y  following the condification
         if(self.validParticule()):
             supportRegle = np.array([False for _ in range(df.shape[0])])
             supportPremis = np.array([False for _ in range(df.shape[0])])
-            if(self.lift):
-                supportConclusion = np.array([False for _ in range(df.shape[0])])
+
+            supportConclusion = np.array([False for _ in range(df.shape[0])])
 
             enter = False
+            firstConclusion = False
+            firstPremis = False
             # this conditon is used when we try to get the fitness of new postion
             if(postion):
                 particule=self.position
@@ -34,44 +43,54 @@ class Particule:
                 if (particule[i] == 1):
                     if (particule[i + 1] == 0):
                         # enter the if (particule[i]==1) for the first time
-                        if (not enter):
+                        if (not firstPremis):
+                            firstPremis = True
                             # for calculation sup(X)  in X=>Y
-                            supportPremis = np.array(df.iloc[:, index] == 1)
+                            supportPremis = np.array(df.iloc[:, index])
                         else:
                             # for calculation sup(X)  in X=>Y
-                            supportPremis = supportPremis & np.array(df.iloc[:, index] == 1)
-                    elif (self.lift):
+                            supportPremis = supportPremis & np.array(df.iloc[:, index])
 
-                        if (not enter):
-                            # for calculation sup(Y)  in X=>Y
-                            supportConclusion = np.array(df.iloc[:, index] == 1)
-                        else:
-                            # for calculation sup(Y)  in X=>Y
-                            supportConclusion = supportConclusion & np.array(df.iloc[:, index] == 1)
-
-                    # enter the if (particule[i]==1) for the first time
-                    if (not enter):
-                        # for calculation sup(X=>Y)  in X=>Y
-                        supportRegle = np.array(df.iloc[:, index] == 1)
-                        enter = True
                     else:
-                        # for calculation sup(X=>Y)  in X=>Y
-                        supportRegle = supportRegle & np.array(df.iloc[:, index] == 1)
-            supportRegle=(supportRegle==True).sum() / df.shape[0]
-            supportPremis=(supportPremis==True).sum() / df.shape[0]
+                        if (not firstConclusion):
+                            firstConclusion = True
+                            supportConclusion = np.array(df.iloc[:, index])
+                        else:
+                            supportConclusion = supportConclusion & np.array(df.iloc[:, index])
+
+            supportRegle = supportPremis & supportConclusion
+            supportRegle=(supportRegle).sum() / df.shape[0]
+            supportPremis=(supportPremis).sum() / df.shape[0]
+            supportConclusion = (supportConclusion).sum() / df.shape[0]
+            if(supportPremis!=0 and supportRegle!=0):
+                 temp_confidence=(supportRegle / supportPremis)
+                 temp_lift = supportRegle/(supportPremis*supportConclusion)
+                 temp_leverage= supportRegle - supportPremis*supportConclusion
+                 if(1-self.confidence==0):
+                    temp_conviction=np.inf
+            if(not postion and supportPremis!=0 and supportRegle!=0):
+                self.confidence = (supportRegle / supportPremis)
+                self.support = supportRegle
+                self.lift = supportRegle/(supportPremis*supportConclusion)
+                self.leverage = supportRegle - supportPremis*supportConclusion
+                if (1 - self.confidence != 0):
+                    self.conviction = (1-supportConclusion)/(1-self.confidence)
+
+
+
+            #print("support conclusion : {}, Premis : {}, regle : {}".format(supportConclusion,supportPremis,supportRegle))
 
             if(supportPremis!=0 and supportRegle!=0):
                 #if the lift is chosen as the fitness musure
-                if(self.lift):
-                    supportConclusion=(supportConclusion==True).sum() / df.shape[0]
-                    if(supportConclusion != 0):
-                        return (supportRegle/(supportPremis*supportConclusion))
-                    else:
-                        return 0
+                if(self.mesure=="confidence"):
+                    return temp_confidence
+                if(self.mesure=="lift"):
+                    return temp_lift
+                if(self.mesure=="leverage"):
+                    return temp_leverage
+                if(self.mesure=="conviction"):
+                    return temp_conviction
 
-                #else we use the support as default
-                else:
-                    return (supportRegle/supportPremis)*supportRegle
             else:
                 return 0
         else:
@@ -91,6 +110,7 @@ class Particule:
           else:
               self.pBest=self.position
               self.pbestFitness=newScore
+              self.fitness(df,postion=False)
           i+=1
 
 
@@ -105,12 +125,10 @@ class Particule:
                 # if particule has the premis part
                 else:
                     premis.append(columns[i / 2])
-        print('(', end='')
-        for pre in premis:
-            print(pre, end=',')
-        print(')=>', end=' ')
-        for con in conclusion:
-            print(con, end=',')
+        #print('(', end='')
+        self.premis = frozenset(premis)
+        self.conclusion= frozenset(conclusion)
+        #print(premis,'=>',conclusion)
 
     def validParticule(self):
         validConclusion = False
@@ -129,7 +147,12 @@ class Particule:
             return False
 
     def equal(self,particule):
-        return ((self.pBest == particule.pBest) == True).sum() == self.size
+        for i in range(0,self.pBest.size,2):
+            if(self.pBest[i]==1):
+                 if(self.pBest[i]!=particule.pBest[i] or self.pBest[i+1]!=particule.pBest[i+1]):
+                     return False
+
+        return True
 
 
 
